4d6bbc219e0234bc82aa372a415f410433ca65bf
Phraeyll <Phraeyll@users.noreply.github.com>
2018-09-01 23:13:03 -0600

    Zero distance parameter added - no implementation yet.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::env;
 fn main() {
     let argv: Vec<String> = env::args().collect();
 
-    if argv.len() <= 15 {
+    if argv.len() <= 16 {
         eprintln!("error: wrong number of args");
         usage(argv[0].to_string());
         return;
@@ -16,18 +16,19 @@ fn main() {
     let initial_velocity: f64 = argv[1].parse().unwrap(); // ft/s
     let launch_angle: f64 = argv[2].parse().unwrap(); // degrees
     let scope_height: f64 = argv[3].parse().unwrap(); // inches
-    let weight: f64 = argv[4].parse().unwrap(); // grains
-    let caliber: f64 = argv[5].parse().unwrap(); // inches
-    let bc: f64 = argv[6].parse().unwrap(); // dimensionless
-    let drag_table: DragTableKind = argv[7].parse().unwrap(); // Desired drag table (G1, G7, etc.)
-    let wind_velocity: f64 = argv[8].parse().unwrap(); // m/h
-    let wind_angle: f64 = argv[9].parse().unwrap(); // degrees
-    let temperature: f64 = argv[10].parse().unwrap(); // F
-    let pressure: f64 = argv[11].parse().unwrap(); // inHg
-    let humidity: f64 = argv[12].parse().unwrap(); // dimensionless, percentage
-    let range: f64 = argv[13].parse().unwrap(); // range in yd
-    let step: f64 = argv[14].parse().unwrap(); // step output in yd
-    let step_factor: f64 = argv[15].parse().unwrap(); // factor to determin step size
+    let zero_distance: f64 = argv[4].parse().unwrap(); // yards
+    let weight: f64 = argv[5].parse().unwrap(); // grains
+    let caliber: f64 = argv[6].parse().unwrap(); // inches
+    let bc: f64 = argv[7].parse().unwrap(); // dimensionless
+    let drag_table: DragTableKind = argv[8].parse().unwrap(); // Desired drag table (G1, G7, etc.)
+    let wind_velocity: f64 = argv[9].parse().unwrap(); // m/h
+    let wind_angle: f64 = argv[10].parse().unwrap(); // degrees
+    let temperature: f64 = argv[11].parse().unwrap(); // F
+    let pressure: f64 = argv[12].parse().unwrap(); // inHg
+    let humidity: f64 = argv[13].parse().unwrap(); // dimensionless, percentage
+    let range: f64 = argv[14].parse().unwrap(); // range in yd
+    let step: f64 = argv[15].parse().unwrap(); // step output in yd
+    let step_factor: f64 = argv[16].parse().unwrap(); // factor to determin step size
 
     let time_step: f64 = 1.0 / (step_factor * initial_velocity);
 
@@ -38,6 +39,7 @@ fn main() {
         initial_velocity,
         launch_angle,
         scope_height,
+        zero_distance,
         drag_table,
         time_step,
         wind_velocity,
