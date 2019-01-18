03912bb27935039d03d26fea68a29c9477c60ac7
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-17 22:52:15 -0700

    Allow lower case enum values - forbid invalid for now, instead of defaulting to G1 (need proper error handling soon)

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,15 @@ fn main() {
     let bc = BallisticCoefficient::new(
         app.value_of("bc").unwrap().parse().unwrap(),
         match app.value_of("bc-type").unwrap() {
-            "G1" => G1,
-            "G2" => G2,
-            "G5" => G5,
-            "G6" => G6,
-            "G7" => G7,
-            "G8" => G8,
-            "GI" => GI,
-            "GS" => GS,
-            _ => G1,
+            "G1" | "g1" => G1,
+            "G2" | "g2" => G2,
+            "G5" | "g5" => G5,
+            "G6" | "g6" => G6,
+            "G7" | "g7" => G7,
+            "G8" | "g8" => G8,
+            "GI" | "gi" => GI,
+            "GS" | "gs" => GS,
+            _ => panic!("bc-type invalid - please use a valid variant <G1 G2 G5 G6 G7 G8 GI GS>"),
         },
     );
 
