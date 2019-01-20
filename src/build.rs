b6f5e1135393e722cdd0ead281a00389eb19b3f2
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-19 18:23:29 -0700

    Rename app to args to better match what it is, and shuffle some imports around

--- src/build.rs
+++ src/build.rs
@@ -1,22 +1,23 @@
 use clap::ArgMatches;
+
 use rballistics_flat::model::point_mass::*;
 
-pub fn from_args(app: &ArgMatches) -> SimulationBuilder {
+pub fn from_args(args: &ArgMatches) -> SimulationBuilder {
     SimulationBuilder::new()
         .time_step(
-            app.value_of("time-step").unwrap().parse().unwrap())
+            args.value_of("time-step").unwrap().parse().unwrap())
         .projectile(
             Projectile::new()
                 .with_velocity(
-                    app.value_of("velocity").unwrap().parse().unwrap())
+                    args.value_of("velocity").unwrap().parse().unwrap())
                 .with_grains(
-                    app.value_of("grains").unwrap().parse().unwrap())
+                    args.value_of("grains").unwrap().parse().unwrap())
                 .with_caliber(
-                    app.value_of("caliber").unwrap().parse().unwrap())
+                    args.value_of("caliber").unwrap().parse().unwrap())
                 .with_bc(
                     BallisticCoefficient::new(
-                        app.value_of("bc").unwrap().parse().unwrap(),
-                        match app.value_of("bc-type").unwrap() {
+                        args.value_of("bc").unwrap().parse().unwrap(),
+                        match args.value_of("bc-type").unwrap() {
                             "G1" | "g1" => G1,
                             "G2" | "g2" => G2,
                             "G5" | "g5" => G5,
@@ -33,44 +34,44 @@ pub fn from_args(app: &ArgMatches) -> SimulationBuilder {
         .scope(
             Scope::new()
                 .with_height(
-                    app.value_of("scope-height").unwrap().parse().unwrap())
+                    args.value_of("scope-height").unwrap().parse().unwrap())
         )
         .solve_conditions(
             Conditions::new()
                 .with_temperature(
-                    app.value_of("temperature").unwrap().parse().unwrap())
+                    args.value_of("temperature").unwrap().parse().unwrap())
                 .with_pressure(
-                    app.value_of("pressure").unwrap().parse().unwrap())
+                    args.value_of("pressure").unwrap().parse().unwrap())
                 .with_humidity(
-                    app.value_of("humidity").unwrap().parse().unwrap())
+                    args.value_of("humidity").unwrap().parse().unwrap())
                 .with_wind_speed(
-                    app.value_of("wind-speed").unwrap().parse().unwrap())
+                    args.value_of("wind-speed").unwrap().parse().unwrap())
                 .with_wind_angle(
-                    app.value_of("wind-angle").unwrap().parse().unwrap())
+                    args.value_of("wind-angle").unwrap().parse().unwrap())
                 .with_shot_angle(
-                    app.value_of("shot-angle").unwrap().parse().unwrap())
+                    args.value_of("shot-angle").unwrap().parse().unwrap())
                 .with_lattitude(
-                    app.value_of("lattitude").unwrap().parse().unwrap())
+                    args.value_of("lattitude").unwrap().parse().unwrap())
                 .with_bearing(
-                    app.value_of("bearing").unwrap().parse().unwrap())
+                    args.value_of("bearing").unwrap().parse().unwrap())
         )
         .zero_conditions(
             Conditions::new()
                 .with_temperature(
-                    app.value_of("zero-temperature").unwrap().parse().unwrap())
+                    args.value_of("zero-temperature").unwrap().parse().unwrap())
                 .with_pressure(
-                    app.value_of("zero-pressure").unwrap().parse().unwrap())
+                    args.value_of("zero-pressure").unwrap().parse().unwrap())
                 .with_humidity(
-                    app.value_of("zero-humidity").unwrap().parse().unwrap())
+                    args.value_of("zero-humidity").unwrap().parse().unwrap())
                 .with_wind_speed(
-                    app.value_of("zero-wind-speed").unwrap().parse().unwrap())
+                    args.value_of("zero-wind-speed").unwrap().parse().unwrap())
                 .with_wind_angle(
-                    app.value_of("zero-wind-angle").unwrap().parse().unwrap())
+                    args.value_of("zero-wind-angle").unwrap().parse().unwrap())
                 .with_shot_angle(
-                    app.value_of("zero-shot-angle").unwrap().parse().unwrap())
+                    args.value_of("zero-shot-angle").unwrap().parse().unwrap())
                 .with_lattitude(
-                    app.value_of("zero-lattitude").unwrap().parse().unwrap())
+                    args.value_of("zero-lattitude").unwrap().parse().unwrap())
                 .with_bearing(
-                    app.value_of("zero-bearing").unwrap().parse().unwrap())
+                    args.value_of("zero-bearing").unwrap().parse().unwrap())
         )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
+use printer::{plain, pretty};
+
 mod build;
 mod cli;
 mod printer;
 
-use printer::{plain, pretty};
-
 fn main() {
-    let app = cli::parse().get_matches();
-    let pretty = app.is_present("pretty");
+    let args = cli::parse().get_matches();
+    let pretty = args.is_present("pretty");
 
-    let builder = build::from_args(&app);
+    let builder = build::from_args(&args);
 
     // let simulation = builder.flat(0.0, 0.0);
     let simulation = builder.solve_for(
-        app.value_of("zero-distance").unwrap().parse().unwrap(),
-        app.value_of("zero-offset").unwrap().parse().unwrap(),
-        app.value_of("zero-tolerance").unwrap().parse().unwrap(),
-        app.value_of("pitch-offset").unwrap().parse().unwrap(),
-        app.value_of("yaw-offset").unwrap().parse().unwrap(),
+        args.value_of("zero-distance").unwrap().parse().unwrap(),
+        args.value_of("zero-offset").unwrap().parse().unwrap(),
+        args.value_of("zero-tolerance").unwrap().parse().unwrap(),
+        args.value_of("pitch-offset").unwrap().parse().unwrap(),
+        args.value_of("yaw-offset").unwrap().parse().unwrap(),
     );
 
     let table = simulation.table(
-        app.value_of("table-step").unwrap().parse().unwrap(),
-        app.value_of("table-start").unwrap().parse().unwrap(),
-        app.value_of("table-end").unwrap().parse().unwrap(),
+        args.value_of("table-step").unwrap().parse().unwrap(),
+        args.value_of("table-start").unwrap().parse().unwrap(),
+        args.value_of("table-end").unwrap().parse().unwrap(),
     );
 
-    let output_tolerance = app.value_of("table-tolerance").unwrap().parse().unwrap();
+    let output_tolerance = args.value_of("table-tolerance").unwrap().parse().unwrap();
 
     if pretty {
         pretty::print(table, output_tolerance);
