ee38feb53540f89fce8fed9224cb041100cf90c1
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-19 17:00:06 -0700

    Move builder into separate function

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,19 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use rballistics_flat::model::point_mass::*;
+pub use self::Adjustment::*;
+
+pub enum Adjustment<'n> {
+    Elevation(&'n Numeric),
+    Windage(&'n Numeric),
+}
 
 fn main() {
     let app = cli().get_matches();
-
     let pretty = app.is_present("pretty");
 
-    let initial_velocity = app.value_of("velocity").unwrap().parse().unwrap();
-    let factor: Numeric = app.value_of("factor").unwrap().parse().unwrap();
-    let time_step: Numeric = 1.0 / (factor * initial_velocity);
+    let builder = build_from_args(&app);
 
-    let builder = SimulationBuilder::new()
-        .time_step(time_step)
-        .projectile(
-            Projectile::new()
-                .with_velocity(initial_velocity)
-                .with_grains(
-                    app.value_of("grains").unwrap().parse().unwrap())
-                .with_caliber(
-                    app.value_of("caliber").unwrap().parse().unwrap())
-                .with_bc(
-                    BallisticCoefficient::new(
-                        app.value_of("bc").unwrap().parse().unwrap(),
-                        match app.value_of("bc-type").unwrap() {
-                            "G1" | "g1" => G1,
-                            "G2" | "g2" => G2,
-                            "G5" | "g5" => G5,
-                            "G6" | "g6" => G6,
-                            "G7" | "g7" => G7,
-                            "G8" | "g8" => G8,
-                            "GI" | "gi" => GI,
-                            "GS" | "gs" => GS,
-                            _ => panic!("bc-type invalid - please use a valid variant <G1 G2 G5 G6 G7 G8 GI GS>"),
-                        },
-                    )
-                )
-        )
-        .scope(
-            Scope::new()
-                .with_height(
-                    app.value_of("scope-height").unwrap().parse().unwrap())
-        )
-        .solve_conditions(
-            Conditions::new()
-                .with_temperature(
-                    app.value_of("temperature").unwrap().parse().unwrap())
-                .with_pressure(
-                    app.value_of("pressure").unwrap().parse().unwrap())
-                .with_humidity(
-                    app.value_of("humidity").unwrap().parse().unwrap())
-                .with_wind_speed(
-                    app.value_of("wind-speed").unwrap().parse().unwrap())
-                .with_wind_angle(
-                    app.value_of("wind-angle").unwrap().parse().unwrap())
-                .with_shot_angle(
-                    app.value_of("shot-angle").unwrap().parse().unwrap())
-                .with_lattitude(
-                    app.value_of("lattitude").unwrap().parse().unwrap())
-                .with_bearing(
-                    app.value_of("bearing").unwrap().parse().unwrap())
-        )
-        .zero_conditions(
-            Conditions::new()
-                .with_temperature(
-                    app.value_of("zero-temperature").unwrap().parse().unwrap())
-                .with_pressure(
-                    app.value_of("zero-pressure").unwrap().parse().unwrap())
-                .with_humidity(
-                    app.value_of("zero-humidity").unwrap().parse().unwrap())
-                .with_wind_speed(
-                    app.value_of("zero-wind-speed").unwrap().parse().unwrap())
-                .with_wind_angle(
-                    app.value_of("zero-wind-angle").unwrap().parse().unwrap())
-                .with_shot_angle(
-                    app.value_of("zero-shot-angle").unwrap().parse().unwrap())
-                .with_lattitude(
-                    app.value_of("zero-lattitude").unwrap().parse().unwrap())
-                .with_bearing(
-                    app.value_of("zero-bearing").unwrap().parse().unwrap())
-        )
-    ;
-
+    // let simulation = builder.flat(0.0, 0.0);
     let simulation = builder.solve_for(
         app.value_of("zero-distance").unwrap().parse().unwrap(),
         app.value_of("zero-offset").unwrap().parse().unwrap(),
@@ -88,7 +21,6 @@ fn main() {
         app.value_of("pitch-offset").unwrap().parse().unwrap(),
         app.value_of("yaw-offset").unwrap().parse().unwrap(),
     );
-    // let simulation = builder.flat(0.0, 0.0);
 
     let table = simulation.table(
         app.value_of("table-step").unwrap().parse().unwrap(),
@@ -180,12 +112,6 @@ fn main() {
     }
 }
 
-pub use self::Adjustment::*;
-pub enum Adjustment<'n> {
-    Elevation(&'n Numeric),
-    Windage(&'n Numeric),
-}
-
 impl Adjustment<'_> {
     fn adjustment(&self, output_tolerance: Numeric) -> char {
         let tolerance = output_tolerance;
@@ -210,6 +136,80 @@ impl Adjustment<'_> {
             }
         }
     }
+}
+fn build_from_args(app: &ArgMatches) -> SimulationBuilder {
+    let initial_velocity = app.value_of("velocity").unwrap().parse().unwrap();
+    let factor: Numeric = app.value_of("factor").unwrap().parse().unwrap();
+    let time_step: Numeric = 1.0 / (factor * initial_velocity);
+    SimulationBuilder::new()
+        .time_step(time_step)
+        .projectile(
+            Projectile::new()
+                .with_velocity(initial_velocity)
+                .with_grains(
+                    app.value_of("grains").unwrap().parse().unwrap())
+                .with_caliber(
+                    app.value_of("caliber").unwrap().parse().unwrap())
+                .with_bc(
+                    BallisticCoefficient::new(
+                        app.value_of("bc").unwrap().parse().unwrap(),
+                        match app.value_of("bc-type").unwrap() {
+                            "G1" | "g1" => G1,
+                            "G2" | "g2" => G2,
+                            "G5" | "g5" => G5,
+                            "G6" | "g6" => G6,
+                            "G7" | "g7" => G7,
+                            "G8" | "g8" => G8,
+                            "GI" | "gi" => GI,
+                            "GS" | "gs" => GS,
+                            _ => panic!("bc-type invalid - please use a valid variant <G1 G2 G5 G6 G7 G8 GI GS>"),
+                        },
+                    )
+                )
+        )
+        .scope(
+            Scope::new()
+                .with_height(
+                    app.value_of("scope-height").unwrap().parse().unwrap())
+        )
+        .solve_conditions(
+            Conditions::new()
+                .with_temperature(
+                    app.value_of("temperature").unwrap().parse().unwrap())
+                .with_pressure(
+                    app.value_of("pressure").unwrap().parse().unwrap())
+                .with_humidity(
+                    app.value_of("humidity").unwrap().parse().unwrap())
+                .with_wind_speed(
+                    app.value_of("wind-speed").unwrap().parse().unwrap())
+                .with_wind_angle(
+                    app.value_of("wind-angle").unwrap().parse().unwrap())
+                .with_shot_angle(
+                    app.value_of("shot-angle").unwrap().parse().unwrap())
+                .with_lattitude(
+                    app.value_of("lattitude").unwrap().parse().unwrap())
+                .with_bearing(
+                    app.value_of("bearing").unwrap().parse().unwrap())
+        )
+        .zero_conditions(
+            Conditions::new()
+                .with_temperature(
+                    app.value_of("zero-temperature").unwrap().parse().unwrap())
+                .with_pressure(
+                    app.value_of("zero-pressure").unwrap().parse().unwrap())
+                .with_humidity(
+                    app.value_of("zero-humidity").unwrap().parse().unwrap())
+                .with_wind_speed(
+                    app.value_of("zero-wind-speed").unwrap().parse().unwrap())
+                .with_wind_angle(
+                    app.value_of("zero-wind-angle").unwrap().parse().unwrap())
+                .with_shot_angle(
+                    app.value_of("zero-shot-angle").unwrap().parse().unwrap())
+                .with_lattitude(
+                    app.value_of("zero-lattitude").unwrap().parse().unwrap())
+                .with_bearing(
+                    app.value_of("zero-bearing").unwrap().parse().unwrap())
+        )
 }
 fn cli<'a, 'b>() -> App<'a, 'b> {
     App::new("Ballistic Solver")
