1d571bc9475cc5c27252c2becb05e81723816047
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-21 10:41:29 -0700

    Add some defaults - weird to input floating points as strs.  May reconsider

--- src/build.rs
+++ src/build.rs
@@ -5,23 +5,23 @@ use rballistics_flat::model::point_mass::*;
 pub fn from_args(args: &ArgMatches) -> Solver {
     Solver::new()
         .time_step(
-            args.value_of("time-step").unwrap().parse().unwrap())
+            args.value_of("time-step").unwrap_or("0.00005").parse().unwrap())
                 .expect("time-step")
         .projectile(
             Projectile::new()
                 .with_velocity(
-                    args.value_of("velocity").unwrap().parse().unwrap())
+                    args.value_of("velocity").unwrap_or("2710").parse().unwrap())
                     .expect("velocity")
                 .with_grains(
-                    args.value_of("grains").unwrap().parse().unwrap())
+                    args.value_of("grains").unwrap_or("140").parse().unwrap())
                     .expect("grains")
                 .with_caliber(
-                    args.value_of("caliber").unwrap().parse().unwrap())
+                    args.value_of("caliber").unwrap_or("0.264").parse().unwrap())
                     .expect("caliber")
                 .with_bc(
                     BallisticCoefficient::new(
-                        args.value_of("bc").unwrap().parse().unwrap(),
-                        match args.value_of("bc-type").unwrap() {
+                        args.value_of("bc").unwrap_or("0.305").parse().unwrap(),
+                        match args.value_of("bc-type").unwrap_or("g7") {
                             "G1" | "g1" => G1,
                             "G2" | "g2" => G2,
                             "G5" | "g5" => G5,
@@ -39,62 +39,62 @@ pub fn from_args(args: &ArgMatches) -> Solver {
         .scope(
             Scope::new()
                 .with_height(
-                    args.value_of("scope-height").unwrap().parse().unwrap())
+                    args.value_of("scope-height").unwrap_or("1.5").parse().unwrap())
                 .with_offset(
-                    args.value_of("scope-offset").unwrap().parse().unwrap())
+                    args.value_of("scope-offset").unwrap_or("0.0").parse().unwrap())
         )
         .solve_conditions(
             Conditions::new()
                 .with_temperature(
-                    args.value_of("temperature").unwrap().parse().unwrap())
+                    args.value_of("temperature").unwrap_or("68").parse().unwrap())
                     .expect("temperature")
                 .with_pressure(
-                    args.value_of("pressure").unwrap().parse().unwrap())
+                    args.value_of("pressure").unwrap_or("29.92").parse().unwrap())
                     .expect("pressure")
                 .with_humidity(
-                    args.value_of("humidity").unwrap().parse().unwrap())
+                    args.value_of("humidity").unwrap_or("0").parse().unwrap())
                     .expect("humidity")
                 .with_wind_speed(
-                    args.value_of("wind-speed").unwrap().parse().unwrap())
+                    args.value_of("wind-speed").unwrap_or("0").parse().unwrap())
                     .expect("wind-speed")
                 .with_wind_angle(
-                    args.value_of("wind-angle").unwrap().parse().unwrap())
+                    args.value_of("wind-angle").unwrap_or("0").parse().unwrap())
                     .expect("wind-angle")
                 .with_shot_angle(
-                    args.value_of("shot-angle").unwrap().parse().unwrap())
+                    args.value_of("shot-angle").unwrap_or("0").parse().unwrap())
                     .expect("shot-angle")
                 .with_lattitude(
-                    args.value_of("lattitude").unwrap().parse().unwrap())
+                    args.value_of("lattitude").unwrap_or("0").parse().unwrap())
                     .expect("lattitude")
                 .with_bearing(
-                    args.value_of("bearing").unwrap().parse().unwrap())
+                    args.value_of("bearing").unwrap_or("0").parse().unwrap())
                     .expect("bearing")
         )
         .zero_conditions(
             Conditions::new()
                 .with_temperature(
-                    args.value_of("zero-temperature").unwrap().parse().unwrap())
+                    args.value_of("zero-temperature").unwrap_or("68").parse().unwrap())
                     .expect("zero-temperature")
                 .with_pressure(
-                    args.value_of("zero-pressure").unwrap().parse().unwrap())
+                    args.value_of("zero-pressure").unwrap_or("29.92").parse().unwrap())
                     .expect("zero-pressure")
                 .with_humidity(
-                    args.value_of("zero-humidity").unwrap().parse().unwrap())
+                    args.value_of("zero-humidity").unwrap_or("0").parse().unwrap())
                     .expect("zero-humidity")
                 .with_wind_speed(
-                    args.value_of("zero-wind-speed").unwrap().parse().unwrap())
+                    args.value_of("zero-wind-speed").unwrap_or("0").parse().unwrap())
                     .expect("zero-wind-speed")
                 .with_wind_angle(
-                    args.value_of("zero-wind-angle").unwrap().parse().unwrap())
+                    args.value_of("zero-wind-angle").unwrap_or("0").parse().unwrap())
                     .expect("zero-wind-angle")
                 .with_shot_angle(
-                    args.value_of("zero-shot-angle").unwrap().parse().unwrap())
+                    args.value_of("zero-shot-angle").unwrap_or("0").parse().unwrap())
                     .expect("zero-shot-angle")
                 .with_lattitude(
-                    args.value_of("zero-lattitude").unwrap().parse().unwrap())
+                    args.value_of("zero-lattitude").unwrap_or("0").parse().unwrap())
                     .expect("zero-lattitude")
                 .with_bearing(
-                    args.value_of("zero-bearing").unwrap().parse().unwrap())
+                    args.value_of("zero-bearing").unwrap_or("0").parse().unwrap())
                     .expect("zero-bearing")
         )
 }
--- src/cli.rs
+++ src/cli.rs
@@ -26,35 +26,30 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
         .arg(
             Arg::with_name("velocity")
                 .long("velocity")
-                .required(true)
                 .takes_value(true)
                 .help("Projectile Velocity (ft/s)"),
         )
         .arg(
             Arg::with_name("grains")
                 .long("grains")
-                .required(true)
                 .takes_value(true)
                 .help("Projectile Weight (grains)"),
         )
         .arg(
             Arg::with_name("caliber")
                 .long("caliber")
-                .required(true)
                 .takes_value(true)
                 .help("Caliber (inches)"),
         )
         .arg(
             Arg::with_name("bc")
                 .long("bc")
-                .required(true)
                 .takes_value(true)
                 .help("Ballistic Coefficient"),
         )
         .arg(
             Arg::with_name("bc-type")
                 .long("bc-type")
-                .required(true)
                 .takes_value(true)
                 .help("BC Type [G1 G2 G5 G6 G7 G8 GI GS]"),
         )
@@ -62,7 +57,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("scope-height")
                 .allow_hyphen_values(true)
                 .long("scope-height")
-                .required(true)
                 .takes_value(true)
                 .help("Scope Height above/below Boreline (Inches)"),
         )
@@ -70,14 +64,12 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("scope-offset")
                 .allow_hyphen_values(true)
                 .long("scope-offset")
-                .required(true)
                 .takes_value(true)
                 .help("Scope Offset left/right of Boreline (Inches)"),
         )
         .arg(
             Arg::with_name("wind-speed")
                 .long("wind-speed")
-                .required(true)
                 .takes_value(true)
                 .help("Wind Speed (miles/hour)"),
         )
@@ -85,14 +77,12 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("wind-angle")
                 .allow_hyphen_values(true)
                 .long("wind-angle")
-                .required(true)
                 .takes_value(true)
                 .help("Wind Angle (Decimal Degrees)"),
         )
         .arg(
             Arg::with_name("zero-wind-speed")
                 .long("zero-wind-speed")
-                .required(true)
                 .takes_value(true)
                 .help("Wind Speed (miles/hour)"),
         )
@@ -100,7 +90,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("zero-wind-angle")
                 .allow_hyphen_values(true)
                 .long("zero-wind-angle")
-                .required(true)
                 .takes_value(true)
                 .help("Wind Angle (Decimal Degrees)"),
         )
@@ -108,7 +97,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("temperature")
                 .allow_hyphen_values(true)
                 .long("temperature")
-                .required(true)
                 .takes_value(true)
                 .help("Temperature (Fahrenheit)"),
         )
@@ -116,14 +104,12 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("pressure")
                 .allow_hyphen_values(true)
                 .long("pressure")
-                .required(true)
                 .takes_value(true)
                 .help("Pressure (InHg)"),
         )
         .arg(
             Arg::with_name("humidity")
                 .long("humidity")
-                .required(true)
                 .takes_value(true)
                 .help("Humidity (Value between 0 & 1) [0 => 0%; 1 => 100%]"),
         )
@@ -131,7 +117,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("zero-temperature")
                 .allow_hyphen_values(true)
                 .long("zero-temperature")
-                .required(true)
                 .takes_value(true)
                 .help("Temperature (Fahrenheit)"),
         )
@@ -139,14 +124,12 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("zero-pressure")
                 .allow_hyphen_values(true)
                 .long("zero-pressure")
-                .required(true)
                 .takes_value(true)
                 .help("Pressure (InHg)"),
         )
         .arg(
             Arg::with_name("zero-humidity")
                 .long("zero-humidity")
-                .required(true)
                 .takes_value(true)
                 .help("Humidity (Value between 0 & 1) [0 => 0%; 1 => 100%]"),
         )
@@ -154,7 +137,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("lattitude")
                 .allow_hyphen_values(true)
                 .long("lattitude")
-                .required(true)
                 .takes_value(true)
                 .help("Lattitude (Decimal Degrees)"),
         )
@@ -162,7 +144,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("bearing")
                 .allow_hyphen_values(true)
                 .long("bearing")
-                .required(true)
                 .takes_value(true)
                 .help("Azimuthal Bearing (Decimal Degrees)"),
         )
@@ -170,7 +151,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("shot-angle")
                 .allow_hyphen_values(true)
                 .long("shot-angle")
-                .required(true)
                 .takes_value(true)
                 .help("Line of Sight Angle (Decimal Degrees)"),
         )
@@ -178,7 +158,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("zero-lattitude")
                 .allow_hyphen_values(true)
                 .long("zero-lattitude")
-                .required(true)
                 .takes_value(true)
                 .help("Lattitude (Decimal Degrees)"),
         )
@@ -186,7 +165,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("zero-bearing")
                 .allow_hyphen_values(true)
                 .long("zero-bearing")
-                .required(true)
                 .takes_value(true)
                 .help("Azimuthal Bearing (Decimal Degrees)"),
         )
@@ -194,37 +172,32 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("zero-shot-angle")
                 .allow_hyphen_values(true)
                 .long("zero-shot-angle")
-                .required(true)
                 .takes_value(true)
                 .help("Line of Sight Angle (Decimal Degrees)"),
         )
         .arg(
             Arg::with_name("zero-distance")
                 .long("zero-distance")
-                .required(true)
                 .takes_value(true)
                 .help("Zeroed Distance (Yards)"),
         )
         .arg(
-            Arg::with_name("zero-elevation-offset")
+            Arg::with_name("zero-height")
                 .allow_hyphen_values(true)
-                .long("zero-elevation-offset")
-                .required(true)
+                .long("zero-height")
                 .takes_value(true)
-                .help("Vertical Zero Offset (Inches)"),
+                .help("Vertical Zero Offset (height) (Inches)"),
         )
         .arg(
-            Arg::with_name("zero-windage-offset")
+            Arg::with_name("zero-offset")
                 .allow_hyphen_values(true)
-                .long("zero-windage-offset")
-                .required(true)
+                .long("zero-offset")
                 .takes_value(true)
-                .help("Horizontal Zero Offset (Inches)"),
+                .help("Horizontal Zero Offset (windage) (Inches)"),
         )
         .arg(
             Arg::with_name("zero-tolerance")
                 .long("zero-tolerance")
-                .required(true)
                 .takes_value(true)
                 .help("Zero Tolerance (Inches)"),
         )
@@ -232,7 +205,6 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("pitch-offset")
                 .allow_hyphen_values(true)
                 .long("pitch-offset")
-                .required(true)
                 .takes_value(true)
                 .help("Pitch Offset (MOA - Minutes of Angle)"),
         )
@@ -240,42 +212,36 @@ pub fn parse<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("yaw-offset")
                 .allow_hyphen_values(true)
                 .long("yaw-offset")
-                .required(true)
                 .takes_value(true)
                 .help("Yaw Offset (MOA - Minutes of Angle)"),
         )
         .arg(
             Arg::with_name("table-start")
                 .long("table-start")
-                .required(true)
                 .takes_value(true)
                 .help("Table Start Distance (Yards)"),
         )
         .arg(
             Arg::with_name("table-end")
                 .long("table-end")
-                .required(true)
                 .takes_value(true)
                 .help("Table End Distance (Yards)"),
         )
         .arg(
             Arg::with_name("table-step")
                 .long("table-step")
-                .required(true)
                 .takes_value(true)
                 .help("Table Step Distance (Yards)"),
         )
         .arg(
             Arg::with_name("table-tolerance")
                 .long("table-tolerance")
-                .required(true)
                 .takes_value(true)
                 .help("Table Adjustments Tolerance (Inches)"),
         )
         .arg(
             Arg::with_name("time-step")
                 .long("time-step")
-                .required(true)
                 .takes_value(true)
                 .help(
                     "Simulation Time Step (smaller numbers for slower, more accurate simulation)",
