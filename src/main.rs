2ae40c04c826dec44ad9c383feea2ae853083073
Phraeyll <Phraeyll@users.noreply.github.com>
2019-11-21 22:38:19 -0700

    Added flat model back.  TODO: fix time macro (make semicolons optional somehow?)

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use cli::args::options::Options;
 
 use std::time::Instant;
 
-use point_mass_ballistics::Error;
+use point_mass_ballistics::{radian, Angle, Error};
 use structopt::StructOpt;
 
 macro_rules! time {
-    ($name:expr, {$($stmt:stmt;)+}) => {
+    ($name:expr, {$($stmt:stmt)+}) => {
         let start = Instant::now();
         $(
             $stmt
@@ -20,16 +20,17 @@ fn main() -> Result<(), Error> {
         time!("Parsing Options", {
             let opt = Options::from_args();
         });
-        time!("Building Builders", {
-            let zero_builder = opt.shared_params()?;
-            let firing_builder = opt.shared_params()?;
-        });
-        time!("Zeroing Simulation", {
-            let zero_simulation = opt.zero_scenario(zero_builder)?;
-            let (pitch, yaw) = opt.try_zero(zero_simulation)?;
-        });
+        let mut angles = (Angle::new::<radian>(0.0), Angle::new::<radian>(0.0));
+        if !opt.flags().flat() {
+            time!("Zeroing Simulation", {
+                let zero_builder = opt.shared_params()?;
+                let zero_simulation = opt.zero_scenario(zero_builder)?;
+                angles = opt.try_zero(zero_simulation)?;
+            });
+        }
         time!("Firing Simulation", {
-            let firing_simulation = opt.firing_scenario(firing_builder, pitch, yaw)?;
+            let firing_builder = opt.shared_params()?;
+            let firing_simulation = opt.firing_scenario(firing_builder, angles.0, angles.1)?;
             opt.print(&firing_simulation);
         });
     });
