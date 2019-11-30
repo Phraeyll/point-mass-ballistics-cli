a2d26dbd7491cb2adc209838f4b9192a58fb9f08
Phraeyll <Phraeyll@users.noreply.github.com>
2019-11-29 22:51:36 -0700

    Model time! macro after dbg in std library - bit more verbose, probably better

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 use cli::args::options::Options;
 
-use std::time::Instant;
+use std::{stringify, time::Instant};
 
 use point_mass_ballistics::{radian, Angle, Error};
 use structopt::StructOpt;
 
 macro_rules! time {
-    ($name:expr, {$($stmt:stmt)+}) => {
+    ($expr:expr) => {{
         let start = Instant::now();
-        $(
-            $stmt
-        )+
-        println!("Finished '{}' in: {:#?}", $name, start.elapsed());
-    };
+        match $expr {
+            tmp => {
+                println!("Finished '{}' in: {:#?}", stringify!($expr), start.elapsed());
+                tmp
+            }
+        }
+    }};
 }
 
 fn main() -> Result<(), Error> {
-    time!("Entire Program", {
-        time!("Parsing Options", {
-            let opt = Options::from_args();
-        });
+    time!({
+        let opt = time!(Options::from_args());
         let mut angles = (Angle::new::<radian>(0.0), Angle::new::<radian>(0.0));
         if !opt.flags().flat() {
-            time!("Zeroing Simulation", {
+            time!({
                 let zero_builder = opt.shared_params()?;
                 let zero_simulation = opt.zero_scenario(zero_builder)?;
                 angles = opt.try_zero(zero_simulation)?;
             });
         }
-        time!("Firing Simulation", {
+        time!({
             let firing_builder = opt.shared_params()?;
             let firing_simulation = opt.firing_scenario(firing_builder, angles.0, angles.1)?;
             opt.print(&firing_simulation);
