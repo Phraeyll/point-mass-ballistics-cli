6c3f001e222e51b0e1b26b94552af74f65c010fa
Phraeyll <Phraeyll@users.noreply.github.com>
2020-02-28 00:40:03 -0700

    Simplify adjustment character code

--- src/printer.rs
+++ src/printer.rs
@@ -1,38 +1,27 @@
-pub use self::Adjustment::*;
-
 use point_mass_ballistics::Length;
 
 pub mod plain;
 pub mod pretty;
 
-pub enum Adjustment<'n> {
-    Elevation(&'n Length),
-    Windage(&'n Length),
+#[derive(Clone, Copy)]
+pub enum Adjustment {
+    Elevation(Length),
+    Windage(Length),
 }
 
 // Show needed adjustments to correct shot
-impl Adjustment<'_> {
-    pub fn adjustment(&self, output_tolerance: Length) -> char {
-        let tolerance = output_tolerance;
-        match self {
-            Elevation(&m) => {
-                if m > -tolerance && m < tolerance {
-                    '*'
-                } else if m.is_sign_positive() {
-                    'D'
-                } else {
-                    'U'
-                }
-            }
-            Windage(&m) => {
-                if m > -tolerance && m < tolerance {
-                    '*'
-                } else if m.is_sign_positive() {
-                    'L'
-                } else {
-                    'R'
-                }
-            }
+impl Adjustment {
+    pub fn adjustment(&self, tolerance: Length) -> char {
+        let (n, positive, negative) = match *self {
+            Self::Elevation(n) => (n, 'D', 'U'),
+            Self::Windage(n) => (n, 'L', 'R'),
+        };
+        if n > tolerance {
+            positive
+        } else if n < -tolerance {
+            negative
+        } else {
+            '*'
         }
     }
 }
