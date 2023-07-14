ce5c4e620db148dfc9d6cb458eb7f86b3f490647
Phraeyll <Phraeyll@users.noreply.github.com>
2023-07-14 14:35:56 -0600

    Use named arguments for clarity

--- src/printer.rs
+++ src/printer.rs
@@ -21,57 +21,57 @@ pub fn print_table(
     } else {
         ("", "", "\n")
     };
+    let distance = "Distance(yd)";
+    let elevation = "Drop(in)";
+    let elevation_moa = "Drop(MOA)";
+    let windage = "Wind(in)";
+    let windage_moa = "Wind(MOA)";
+    let velocity = "Velocity(ft/s)";
+    let mach = "Mach";
+    let energy = "Energy(ftlb)";
+    let acceleration = "Acceleration(ft/s^2)";
+    let time = "Time(s)";
     print!(
         "\
         {rs}\
-        {fs}{:>12} \
-        {fs}{:>13} \
-        {fs}{:>10} \
-        {fs}{:>11} \
-        {fs}{:>10} \
-        {fs}{:>14} \
-        {fs}{:>10} \
-        {fs}{:>12} \
-        {fs}{:>20} \
-        {fs}{:>8}{eol}\
+        {fs}{distance:>12} \
+        {fs}{elevation:>13} \
+        {fs}{elevation_moa:>10} \
+        {fs}{windage:>11} \
+        {fs}{windage_moa:>10} \
+        {fs}{velocity:>14} \
+        {fs}{mach:>10} \
+        {fs}{energy:>12} \
+        {fs}{acceleration:>20} \
+        {fs}{time:>8}{eol}\
         {rs}\
-        ",
-        "Distance(yd)",
-        "Drop(in)",
-        "Drop(MOA)",
-        "Wind(in)",
-        "Wind(MOA)",
-        "Velocity(ft/s)",
-        "Mach",
-        "Energy(ftlb)",
-        "Acceleration(ft/s^2)",
-        "Time(s)",
+        "
     );
     for p in iter {
+        let distance = p.distance().get::<yard>();
+        let elevation = p.elevation().get::<inch>();
+        let elevation_moa = p.vertical_angle(output_tolerance).get::<minute>();
+        let windage = p.windage().get::<inch>();
+        let windage_moa = p.horizontal_angle(output_tolerance).get::<minute>();
+        let velocity = p.velocity().get::<foot_per_second>();
+        let mach = p.mach().value;
+        let energy = p.energy().get::<foot_pound>();
+        let acceleration = p.acceleration().get::<foot_per_second_squared>();
+        let time = p.time().get::<second>();
         print!(
             "\
-            {fs}{:>12.precision$} \
-            {fs}{:>13.precision$} \
-            {fs}{:>10.precision$} \
-            {fs}{:>11.precision$} \
-            {fs}{:>10.precision$} \
-            {fs}{:>14.precision$} \
-            {fs}{:>10.precision$} \
-            {fs}{:>12.precision$} \
-            {fs}{:>20.precision$} \
-            {fs}{:>8.3}{eol}\
+            {fs}{distance:>12.precision$} \
+            {fs}{elevation:>13.precision$} \
+            {fs}{elevation_moa:>10.precision$} \
+            {fs}{windage:>11.precision$} \
+            {fs}{windage_moa:>10.precision$} \
+            {fs}{velocity:>14.precision$} \
+            {fs}{mach:>10.precision$} \
+            {fs}{energy:>12.precision$} \
+            {fs}{acceleration:>20.precision$} \
+            {fs}{time:>8.3}{eol}\
             {rs}\
-            ",
-            p.distance().get::<yard>(),
-            p.elevation().get::<inch>(),
-            p.vertical_angle(output_tolerance).get::<minute>(),
-            p.windage().get::<inch>(),
-            p.horizontal_angle(output_tolerance).get::<minute>(),
-            p.velocity().get::<foot_per_second>(),
-            p.mach().value,
-            p.energy().get::<foot_pound>(),
-            p.acceleration().get::<foot_per_second_squared>(),
-            p.time().get::<second>(),
+            "
         );
     }
 }
